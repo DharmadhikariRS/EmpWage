5facbed36cae626d45b78dd48ffbb91e53f3e8bb
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-11 00:26:39 +0530

    [Rushikesh Add:UC1-Employee present Absent]

---
+++ EmpWage/EmpPresentAbsent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    internal class EmpPresentAbsent
+    {
+        public void EmpPresentOrAbsent()
+        {
+            Console.WriteLine("UC1-Employee present or absent");
+
+            Random randomval = new Random();
+
+            int empCheck = randomval.Next(2); // 0 for absent 1 for present.
+
+            Console.WriteLine("empCheck is= " + empCheck);
+            if (empCheck == 1)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmpWage/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+            EmpPresentAbsent uc1 = new EmpPresentAbsent();
+            uc1.EmpPresentOrAbsent();
+
+        }
+    }
+}
